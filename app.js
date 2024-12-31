7e587df33b6b0c333eeb0dec70394857312cad58
BommishettyVamshi <bvamshivamshi22@gmail.com>
2024-12-31 10:20:41 +0000

    completed the full code of this project

--- app.js
+++ app.js
@@ -152,4 +152,151 @@ app.post('/districts/', authenticateToken, async (req, res) => {
   }
 })
 
+//API 5
+app.get('/districts/:districtId/', authenticateToken, async (req, res) => {
+  try {
+    const {districtId} = req.params
+    const selectQuery = `select * from district where district_id = ${districtId};`
 
+    const ans = districtDetails => {
+      return {
+        districtId: districtDetails.district_id,
+        districtName: districtDetails.district_name,
+        stateId: districtDetails.state_id,
+        cases: districtDetails.cases,
+        cured: districtDetails.cured,
+        active: districtDetails.active,
+        deaths: districtDetails.deaths,
+      }
+    }
+
+    const district = await db.get(selectQuery)
+    res.send(ans(district))
+    console.log(
+      `DistrictDetails of districtId: ${districtId} are fetched successfully...`,
+    )
+  } catch (e) {
+    console.log(`Request Error: ${e.message}`)
+  }
+})
+
+//API 6
+app.delete('/districts/:districtId/', authenticateToken, async (req, res) => {
+  try {
+    const {districtId} = req.params
+    const getQuery = `select * from district where district_id = ${districtId};`
+
+    const dbResultOfGetQuery = await db.get(getQuery, (err, row) => {
+      if (err) {
+        return false
+      }
+      if (row) {
+        return true
+      }
+    })
+
+    if (dbResultOfGetQuery) {
+      console.log(
+        `District with dirstictId: ${districtId} is exists in the database`,
+      )
+      const deleteQuery = `delete from district where district_id = ${districtId} ;`
+
+      await db.run(deleteQuery)
+      res.send('District Removed')
+      console.log(
+        `District with districtId: ${districtId} is removed successfully...`,
+      )
+    } else {
+      res.send(
+        `District with districtId: ${districtId} does not exists in the db`,
+      )
+    }
+  } catch (e) {
+    console.log(`Request Error: ${e.message}`)
+  }
+})
+
+//API 7
+app.put('/districts/:districtId/', authenticateToken, async (req, res) => {
+  try {
+    const {districtId} = req.params
+    const {districtName, stateId, cases, cured, active, deaths} = req.body
+    const getQuery = `select * from district where district_id = ${districtId};`
+
+    const dbResultOfGetQuery = await db.get(getQuery, (err, row) => {
+      if (err) {
+        return false
+      }
+      if (row) {
+        return true
+      }
+    })
+
+    if (dbResultOfGetQuery) {
+      console.log(
+        `District with dirstictId: ${districtId} is exists in the database`,
+      )
+      const updateQuery = `update district set district_name = ? , state_id =? , cases = ? , cured = ?, active = ?, deaths = ? where district_id = ? ;`
+      await db.run(updateQuery, [
+        districtName,
+        stateId,
+        cases,
+        cured,
+        active,
+        deaths,
+        districtId,
+      ])
+
+      res.send('District Details Updated')
+      console.log(
+        `District Details of districtId: ${districtId} are updated successfully...`,
+      )
+    } else {
+      console.log(
+        `District with districtId: ${districtId} does not exists in the db`,
+      )
+      res.send(
+        `District with districtId: ${districtId} does not exists in the db`,
+      )
+    }
+  } catch (e) {
+    console.log(`Request Error: ${e.message}`)
+  }
+})
+
+//API 8
+app.get('/states/:stateId/stats/', authenticateToken, async (req, res) => {
+  try {
+    const {stateId} = req.params
+    const getQuery = `select state_id as stateId from state where state_id = ${stateId};`
+
+    const dbResultOfGetQuery = await db.get(getQuery)
+    console.log(dbResultOfGetQuery.stateId)
+
+    const ans = districtStatsDetails => {
+      return {
+        totalCases: districtStatsDetails.totalCases,
+        totalCured: districtStatsDetails.totalCured,
+        totalActive: districtStatsDetails.totalActive,
+        totalDeaths: districtStatsDetails.totalDeaths,
+      }
+    }
+
+    const strStateId = dbResultOfGetQuery.stateId.toString()
+
+    if (dbResultOfGetQuery !== undefined && stateId === strStateId) {
+      console.log(`state with stateId: ${stateId} exists in the db`)
+      const selectQuery = `select sum(d.cases) as totalCases, sum(d.cured) as totalCured, sum(d.active) as totalActive, sum(d.deaths)as totalDeaths from district d join state s on s.state_id = d.state_id where d.state_id = ${stateId};`
+      const dbResult = await db.all(selectQuery)
+      console.log(dbResult)
+      res.send(ans(dbResult[0]))
+      console.log(
+        `Stats of state with state: ${stateId} are fecthed successfully...`,
+      )
+    } else {
+      console.log(`state with stateId: ${stateId} does not exists in the db..`)
+    }
+  } catch (e) {
+    console.log(`Request Error: ${e.message}`)
+  }
+})
